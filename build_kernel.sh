4d535e2b53a38a7b8a85c8d94f20fed785150df1
akhil1999 <akhileshbhuvanendra@gmail.com>
2022-05-03 11:47:43 +0530

    TWRP Kernel Code

---
+++ build_kernel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export CROSS_COMPILE=/home/akhilesh/personalprojects/m32/SM-M325F_SWA_RR_Opensource/toolchain/aarch64-linux-android/bin/aarch64-linux-androidkernel-
+export CC=/home/akhilesh/personalprojects/m32/SM-M325F_SWA_RR_Opensource/toolchain/clang/bin/clang
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export ARCH=arm64
+export ANDROID_MAJOR_VERSION=r
+make distclean
+
+export KCFLAGS=-w
+export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y m32_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j8
+
+cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
